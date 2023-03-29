e68d64dc082e3da273d494b110091fb01c30c0de
PrinceKumar19990 <122788054+PrinceKumar19990@users.noreply.github.com>
2023-03-29 23:46:57 +0530

    Add files via upload

---
+++ Threadeg5.java
@@ -0,0 +1,61 @@
+package Thread;
+//with without using Synchronized
+class ThreadClass{
+	// void print (int num ) { // without Synchronized method 
+	synchronized void print(int num) {
+		for(int i=num;i<=10;i++) {
+			System.out.println(num+i);
+			try {
+				Thread.sleep(500);
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
+class T1 extends Thread{
+	ThreadClass mt;
+	
+	T1(ThreadClass mt){
+		this.mt=mt;
+	}
+	public void run() {
+		mt.print(5);    //5
+	}
+}
+class T2 extends Thread{
+	ThreadClass mt;
+	T2(ThreadClass mt){
+		this.mt=mt;
+	}
+	public void run() {
+		mt.print(3);        //3
+	}
+}
+
+class T3 extends Thread{
+	ThreadClass mt;
+	T3(ThreadClass mt){
+		this.mt=mt;
+	}
+	public void run() {
+		mt.print(4);        //4
+	}
+}
+
+public class Threadeg5 {
+
+	public static void main(String[] args) {
+		ThreadClass obj=new ThreadClass();
+		T1 t1=new T1(obj);
+		T2 t2=new T2(obj);
+		T3 t3=new T3(obj);
+		
+		t1.start();
+		t2.start();
+		t3.start();
+
+	}
+
+}
